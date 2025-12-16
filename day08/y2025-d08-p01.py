fc4b5707a463a099f129813472e891bbcf6d2f60
Mister Cab <mister.h.b.cab@gmail.com>
2025-12-16 00:31:48 -0500

    thank you tina

--- day08/y2025-d08-p01.py
+++ day08/y2025-d08-p01.py
@@ -13,9 +13,25 @@ class DebugPrinter :
 
 ################################
 
+class TheGraph :
+  def __init__(self ) :
+    self.vertecies = set()
+    self.edges = {}
+  
+  def containsV( self, x ) :
+    return x in self.vertecies
+  
+  def combine( self, g2 ) :
+    self.vertecies = self.vertecies.union( g2.vertecies )
+    for e in g2.edges.keys() :
+      self.edges[ e ] = g2.edges[ e ]
+
+#########################
+
 class HauntedWiring :
     
   DEFAULT_FILE = "day08\\input08.txt"
+  #DEFAULT_FILE = "day08\\sample08.txt"
 
   def __init__(self ) :
     self.vertecies = []
@@ -60,17 +76,78 @@ class HauntedWiring :
         j += 1
       i += 1
 
-
   ###########
 
   def get_edge_weight(self, v1, v2) :
     return ( math.sqrt( math.pow(v1[0]-v2[0],2) + math.pow(v1[1]-v2[1],2) + math.pow(v1[2]-v2[2],2) ))
 
+  #############
+
+  # n should default to 1000 for part 1
+  def death_by_1000_edges( self, n ) :
+    clever_algo = []
+    all_edges_list = sorted( self.edges.items(), key=lambda item:item[1] )
+
+    i = 0
+    while i < n :
+      e = all_edges_list[ i ]
+      combine = []
+      
+      j = 0
+
+      # check both vertecies against all graphs
+      for g in clever_algo :
+        if g.containsV( e[0][0] ) or g.containsV( e[0][1] ) :
+          combine.append( j )
+        j += 1
+
+      # neither vertex is in a graph - then make a new graph
+      if len( combine ) == 0  :
+        newbie = TheGraph()
+        newbie.edges[e[0]] = e[1]
+        newbie.vertecies.add( e[0][0] )
+        newbie.vertecies.add( e[0][1] )
+        clever_algo.append( newbie )
+
+      # only add the edge if the graph does not contain both vertecies  
+      elif len( combine ) == 1 :
+        # split out this if to keep the else at the end
+        if not (clever_algo[combine[0] ].containsV(e[0][0]) and clever_algo[combine[0]].containsV(e[0][1] ) ) :
+          clever_algo[ combine[0] ].edges[e[0]] = e[1]
+          clever_algo[combine[0]].vertecies.add( e[0][0] )
+          clever_algo[combine[0]].vertecies.add( e[0][1] )
+        else :
+          DebugPrinter.debugPrint( "Got both already" )
+
+      # doomsday - one vertex is in two graphs
+      # - combine them
+      # - remove the edge
+      # - remove the original graph from the list
+      elif len( combine ) == 2 :
+        clever_algo[combine[0]].combine( clever_algo[combine[1] ] )
+        clever_algo[combine[0]].edges[e[0]] = e[1]
+        del clever_algo[ combine[1] ]
+
+        # the vertecies were already in both graphs
+        
+      # this is an error state
+      else :
+        print( 'How the heck did you get here?!?!?!' )
+        exit (1)
+      i += 1
+
+    #print( clever_algo )
+
+    answer = sorted( [len(x.vertecies) for x in clever_algo])
+    print( str(answer[-1] * answer[-2] * answer[-3] ) )
+
+
 ###############
   def main(self) :
     self.processArguments()
     self.find_every_edge()
-    print ( self.edges )
+    self.death_by_1000_edges( 1000 )
+    #print ( self.edges )
 
 
 #######
